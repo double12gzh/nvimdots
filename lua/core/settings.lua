8602be81be2afa23da4358926e2529b6c5c4dfb2
JeffreyGuan <double12gzh@163.com>
2024-03-24 18:40:02 +0800

    update default theme

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -55,7 +55,7 @@ settings["palette_overwrite"] = {}
 -- Set the colorscheme to use here.
 -- Available values are: `catppuccin`, `catppuccin-latte`, `catppucin-mocha`, `catppuccin-frappe`, `catppuccin-macchiato`.
 ---@type string
-settings["colorscheme"] = "catppuccin"
+settings["colorscheme"] = "gruvbox-material"
 
 -- Set it to true if your terminal has transparent background.
 ---@type boolean
